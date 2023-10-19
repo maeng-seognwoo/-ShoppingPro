9c8e6ec17f75ade86ecce0ded8f66c8b95b11fa8
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-10-19 15:41:52 +0900

    Create Project
    프로젝트 생성

---
+++ ShowpingProject/src/test/java/com/example/ShowpingProject/ShowpingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShowpingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowpingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
